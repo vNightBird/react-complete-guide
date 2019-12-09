48eb956b77c1454adb1e3bc0777df1f56e719156
vNightBird <t.ozalp23@gmail.com>
2019-12-09 20:57:51 +0300

    added button and handler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'something'
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({ persons: [
+      { name: newName, age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 27 }
+    ]}
+    );
+    //NOT RECOMENDED this.state.persons[0].name = 'Tolga';
+    console.log('Was Clicked!');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, Hello World</h1>
+        <p>It works</p>
+        <button onClick={() => this.switchNameHandler('UpdatedWithButton')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'UpdatedWithParagraph')} >My hobbes: Chess</Person>
+        <Person 
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
+        
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1',null,'asdasdas'));
+    //onClick={() => this.switchNameHandler('TTTTT') inefficient
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,1 +1,12 @@
-function person()
+import React from 'react'
+
+const person = ( props ) => {
+    return (
+        <div>
+            <p onClick={ props.click }>Im { props.name } and I am { props.age }</p>
+            <p>{ props.children }</p>
+        </div>
+    )
+};
+
+export default person;
